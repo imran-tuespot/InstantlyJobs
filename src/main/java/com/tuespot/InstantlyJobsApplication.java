4153d74e7c77cdf4d5eb975663d278e5f22e8b0d
imran-tuespot <137031625+imran-tuespot@users.noreply.github.com>
2023-07-08 15:06:00 +0530

    Update InstantlyJobsApplication.java

--- src/main/java/com/tuespot/InstantlyJobsApplication.java
+++ src/main/java/com/tuespot/InstantlyJobsApplication.java
@@ -8,6 +8,8 @@ public class InstantlyJobsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InstantlyJobsApplication.class, args);
+
+		//https://docs.oracle.com/javase/specs/jvms/se20/html/jvms-4.html#jvms-4.1-200-B.2
 	}
 
 }
